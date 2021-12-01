f0c7d74873067fb8197cb5de5f5251081f0f4de8
matteeyao <myaoski@gmail.com>
2021-11-30 21:13:20 -0600

    Move main.go to root

--- main_test.go
+++ main_test.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net"
 	"testing"
 )
 
@@ -40,42 +38,42 @@ func ExampleAdd() {
 	// Output: 10
 }
 
-func TestConn(t *testing.T) {
-	message := "Hi there!\n"
-
-	go func() {
-		conn, err := net.Dial("tcp", "localhost:8080")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer conn.Close()
-
-		if _, err := fmt.Fprintf(conn, message); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	l, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer l.Close()
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			return
-		}
-		defer conn.Close()
-
-		buf, err := ioutil.ReadAll(conn)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		fmt.Println(string(buf[:]))
-		if msg := string(buf[:]); msg != message {
-			t.Fatalf("Unexpected message:\nGot:\t\t%s\nExpected:\t%s\n", msg, message)
-		}
-		return // Done
-	}
-}
+//func TestConn(t *testing.T) {
+//	message := "Hi there!\n"
+//
+//	go func() {
+//		conn, err := net.Dial("tcp", "localhost:8080")
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		defer conn.Close()
+//
+//		if _, err := fmt.Fprintf(conn, message); err != nil {
+//			t.Fatal(err)
+//		}
+//	}()
+//
+//	l, err := net.Listen("tcp", "localhost:8080")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	defer l.Close()
+//	for {
+//		conn, err := l.Accept()
+//		if err != nil {
+//			return
+//		}
+//		defer conn.Close()
+//
+//		buf, err := ioutil.ReadAll(conn)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//
+//		fmt.Println(string(buf[:]))
+//		if msg := string(buf[:]); msg != message {
+//			t.Fatalf("Unexpected message:\nGot:\t\t%s\nExpected:\t%s\n", msg, message)
+//		}
+//		return // Done
+//	}
+//}
